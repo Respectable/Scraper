07da55d62369892f63bf33c6af59ceefc6dfe1ed
Anthony O'Dell <aodell91@gmail.com>
2014-03-14 17:06:08 -0500

    Added box score and shot parsers, tests

---
+++ Scraper/NBA/BBR/Linker/BBRLinkBoxScoreParser.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Scraper.NBA.BBR.Linker
+{
+    public class BBRLinkBoxScoreParser
+    {
+        private string _pattern = Regex.Escape(@"/boxscores/") + @"\d+\w+" + Regex.Escape(@".html");
+        private const string _linkStart = @"www.basketball-reference.com";
+
+        public IEnumerable<Link> ParseString(string text)
+        {
+            var matches = Regex.Matches(text, _pattern);
+
+            var dupsRemoved = from Match m in matches
+                              group m by m.Value into g
+                              select g.First();
+
+            return from Match m in dupsRemoved
+                   select new Link(LinkType.PlayByPlay, _linkStart + m.Value);
+        }
+    }
+}
---
+++ Scraper/NBA/BBR/Linker/BBRLinkShotsParser.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Scraper.NBA.BBR.Linker
+{
+    public class BBRLinkShotsParser
+    {
+        private string _pattern = Regex.Escape(@"/shot-chart/") + @"\d+\w+" + Regex.Escape(@".html");
+        private const string _linkStart = @"www.basketball-reference.com/boxscores";
+
+        public IEnumerable<Link> ParseString(string text)
+        {
+            var matches = Regex.Matches(text, _pattern);
+
+            return from Match m in matches
+                   select new Link(LinkType.PlayByPlay, _linkStart + m.Value);
+        }
+    }
+}
