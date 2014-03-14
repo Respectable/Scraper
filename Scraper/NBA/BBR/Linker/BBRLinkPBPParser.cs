d2c25c25ab61e5394059536989768643a825559c
Anthony O'Dell <aodell91@gmail.com>
2014-03-13 22:48:13 -0500

    Passed first test

--- Scraper/NBA/BBR/Linker/BBRLinkPBPParser.cs
+++ Scraper/NBA/BBR/Linker/BBRLinkPBPParser.cs
@@ -9,14 +9,16 @@ namespace Scraper.NBA.BBR.Linker
 {
     public class BBRLinkPBPParser : IBBRLinkParser
     {
-        private const string _pattern = Regex.Escape("<") + "a href" + Regex.Escape(@"=""/boxscores/pbp/") + @"\d+\w+" + Regex.Escape(@".html"">Play-By-Play");
+        //private const string _pattern = Regex.Escape("<") + "a href" + Regex.Escape(@"=""/boxscores/pbp/") + @"\d+\w+" + Regex.Escape(@".html"">Play-By-Play");
+        private string _pattern = Regex.Escape(@"/pbp/") + @"\d+\w+" + Regex.Escape(@".html");
+        private const string _linkStart = @"www.basketball-reference.com/boxscores";
 
         public IEnumerable<Link> ParseString(string text)
         {
             var matches = Regex.Matches(text, _pattern);
 
             return from Match m in matches
-                   select new Link(LinkType.PlayByPlay, m.Value);
+                   select new Link(LinkType.PlayByPlay, _linkStart + m.Value);
         }
     }
 }
